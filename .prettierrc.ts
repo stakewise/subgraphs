fb5d3a08dae09f8eff882875253d8f11fc84b058
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2021-08-17 18:55:26 +0300

    Add prettier config

---
+++ .prettierrc.ts
@@ -0,0 +1,10 @@
+import { Options } from "prettier";
+
+const options: Options = {
+  singleQuote: true,
+  arrowParens: "avoid",
+  trailingComma: "all",
+  printWidth: 120,
+};
+
+export default options;
