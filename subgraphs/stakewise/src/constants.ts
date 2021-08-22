dac27f85d777addd021bf30637cc08e96dca95da
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2021-08-22 20:48:26 +0300

    Remove redundant empty address constant

--- subgraphs/stakewise/src/constants.ts
+++ subgraphs/stakewise/src/constants.ts
@@ -1,7 +1,4 @@
-import { Address, BigDecimal, Bytes } from "@graphprotocol/graph-ts";
+import { BigDecimal, Bytes } from "@graphprotocol/graph-ts";
 
-export let EMPTY_ADDRESS = Address.fromString(
-  "0x0000000000000000000000000000000000000000"
-);
 export let EMPTY_BIG_DECIMAL = BigDecimal.fromString("0");
 export let EMPTY_BYTES = Bytes.fromI32(0) as Bytes;
