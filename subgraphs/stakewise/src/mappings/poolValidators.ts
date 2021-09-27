b973aecef2a543d047d016fbd1d2549ce8bd18be
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2021-09-27 20:56:42 +0300

    Update pool validators

--- subgraphs/stakewise/src/mappings/poolValidators.ts
+++ subgraphs/stakewise/src/mappings/poolValidators.ts
@@ -1,10 +1,9 @@
-import { log } from "@graphprotocol/graph-ts";
-import { BIG_DECIMAL_1E18, BIG_INT_ZERO, BYTES_ZERO } from "const";
+import { BigInt, log } from "@graphprotocol/graph-ts";
+import { BIG_INT_ZERO, BYTES_ZERO } from "const";
 import {
   createOrLoadOperator,
   createOrLoadNetwork,
   createOrLoadValidator,
-  createOrLoadPool,
 } from "../entities";
 import {
   CollateralDeposited,
@@ -17,13 +16,18 @@ import {
 } from "../../generated/PoolValidators/PoolValidators";
 
 export function handleOperatorAdded(event: OperatorAdded): void {
-  let operator = createOrLoadOperator(event.params.operator.toHexString());
+  let operator = createOrLoadOperator(
+    event.params.operator,
+    event.block.number
+  );
 
   operator.initializeMerkleRoot = event.params.initializeMerkleRoot;
   operator.initializeMerkleProofs = event.params.initializeMerkleProofs;
   operator.finalizeMerkleRoot = event.params.finalizeMerkleRoot;
   operator.finalizeMerkleProofs = event.params.finalizeMerkleProofs;
   operator.depositDataIndex = BIG_INT_ZERO;
+  operator.updatedAtBlock = event.block.number;
+  operator.updatedAtTimestamp = event.block.timestamp;
   operator.save();
 
   log.info(
@@ -39,13 +43,18 @@ export function handleOperatorAdded(event: OperatorAdded): void {
 }
 
 export function handleOperatorRemoved(event: OperatorRemoved): void {
-  let operator = createOrLoadOperator(event.params.operator.toHexString());
+  let operator = createOrLoadOperator(
+    event.params.operator,
+    event.block.number
+  );
 
   operator.initializeMerkleRoot = BYTES_ZERO;
   operator.initializeMerkleProofs = "";
   operator.finalizeMerkleRoot = BYTES_ZERO;
   operator.finalizeMerkleProofs = "";
   operator.depositDataIndex = BIG_INT_ZERO;
+  operator.updatedAtBlock = event.block.number;
+  operator.updatedAtTimestamp = event.block.timestamp;
   operator.save();
 
   log.info("[PoolValidators] OperatorRemoved operator={} sender={}", [
@@ -55,56 +64,67 @@ export function handleOperatorRemoved(event: OperatorRemoved): void {
 }
 
 export function handleOperatorSlashed(event: OperatorSlashed): void {
-  let operator = createOrLoadOperator(event.params.operator.toHexString());
-  let refundedAmount = event.params.refundedAmount.divDecimal(BIG_DECIMAL_1E18);
+  let operator = createOrLoadOperator(
+    event.params.operator,
+    event.block.number
+  );
 
   operator.initializeMerkleRoot = BYTES_ZERO;
   operator.initializeMerkleProofs = "";
   operator.finalizeMerkleRoot = BYTES_ZERO;
   operator.finalizeMerkleProofs = "";
   operator.depositDataIndex = BIG_INT_ZERO;
-  operator.collateral = operator.collateral.minus(refundedAmount);
+  operator.updatedAtBlock = event.block.number;
+  operator.updatedAtTimestamp = event.block.timestamp;
+  operator.collateral = operator.collateral.minus(event.params.refundedAmount);
+  operator.validatorsCount = operator.validatorsCount.minus(
+    BigInt.fromString("1")
+  );
   operator.save();
 
-  let pool = createOrLoadPool();
-  pool.balance = pool.balance.plus(refundedAmount);
-  pool.save();
-
-  let validator = createOrLoadValidator(event.params.publicKey.toHexString());
+  let validator = createOrLoadValidator(event.params.publicKey);
   validator.registrationStatus = "Failed";
   validator.save();
 
   log.info(
     "[PoolValidators] OperatorSlashed operator={} publicKey={} refundAmount={}",
-    [operator.id, validator.id, refundedAmount.toString()]
+    [operator.id, validator.id, event.params.refundedAmount.toString()]
   );
 }
 
 export function handleCollateralDeposited(event: CollateralDeposited): void {
-  let operator = createOrLoadOperator(event.params.operator.toHexString());
-  let collateral = event.params.collateral.divDecimal(BIG_DECIMAL_1E18);
+  let operator = createOrLoadOperator(
+    event.params.operator,
+    event.block.number
+  );
 
-  operator.collateral = operator.collateral.plus(collateral);
+  operator.collateral = operator.collateral.plus(event.params.collateral);
+  operator.updatedAtBlock = event.block.number;
+  operator.updatedAtTimestamp = event.block.timestamp;
   operator.save();
 
   log.info("[PoolValidators] CollateralDeposited operator={} collateral={}", [
     operator.id,
-    collateral.toString(),
+    event.params.collateral.toString(),
   ]);
 }
 
 export function handleCollateralWithdrawn(event: CollateralWithdrawn): void {
-  let operator = createOrLoadOperator(event.params.operator.toHexString());
-  let collateral = event.params.collateral.divDecimal(BIG_DECIMAL_1E18);
+  let operator = createOrLoadOperator(
+    event.params.operator,
+    event.block.number
+  );
 
-  operator.collateral = operator.collateral.minus(collateral);
+  operator.collateral = operator.collateral.minus(event.params.collateral);
+  operator.updatedAtBlock = event.block.number;
+  operator.updatedAtTimestamp = event.block.timestamp;
   operator.save();
 
   log.info(
     "[PoolValidators] CollateralWithdrawn operator={} collateral={} collateralRecipient={}",
     [
       operator.id,
-      collateral.toString(),
+      event.params.collateral.toString(),
       event.params.collateralRecipient.toHexString(),
     ]
   );
