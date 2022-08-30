98109e2852cdefb5b42f0a69741331707d1c9251
Dmitry Pavlov <dmitry.p@stakewise.io>
2022-08-30 17:12:29 +0000

    FeesEscrow for Gnosis & Harbour

--- subgraphs/stakewise/src/mappings/feesEscrow.ts
+++ subgraphs/stakewise/src/mappings/feesEscrow.ts
@@ -1,8 +1,31 @@
-import {createOrLoadPool} from "../entities";
-import { FeesTransferred } from "../../generated/FeesEscrow/FeesEscrow";
+import { createOrLoadPool } from "../entities";
+import { FeesTransferred, FeesTransferred1 } from "../../generated/FeesEscrow/FeesEscrow";
+import { log } from "@graphprotocol/graph-ts";
 
 export function handleFeesTransferred(event: FeesTransferred): void {
     let pool = createOrLoadPool();
     pool.balance = pool.balance.plus(event.params.amount);
     pool.save();
+
+    log.info(
+        "[FeesEscrow] FeesTransferred amount={} poolBalance={}",
+        [
+            event.params.amount.toString(),
+            pool.balance.toString(),
+        ]
+    );
+}
+
+export function handleFeesTransferredGnosis(event: FeesTransferred1): void {
+    let pool = createOrLoadPool();
+    pool.balance = pool.balance.plus(event.params.amountMGNO);
+    pool.save();
+
+    log.info(
+        "[FeesEscrow] FeesTransferred amount={} poolMGNOBalance={}",
+        [
+            event.params.amountMGNO.toString(),
+            pool.balance.toString(),
+        ]
+    );
 }
